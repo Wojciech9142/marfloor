63a0cadefb6cc9f3cb767882fb8724d8bc903535
Wojciech Wolak <wolak.wojciech.fe@gmail.com>
2020-05-17 12:03:55 +0200

    added mail script

---
+++ php/mail.php
@@ -0,0 +1,25 @@
+<?php
+ob_start();
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $telephone = $_POST['telephone'];
+    $message = $_POST['message'];
+
+    echo $name;
+    echo $email;
+    echo $telephone;
+    echo $message;
+
+    $content = nl2br("Nowa wiadomość od ".$name.". \r\n 
+    Adres email do kontaktu: ".$email.", a numer telefonu: ".$telephone."\r\n
+    Wiadomość przesłana brzmi: \r\n".$message);
+
+    $theme = "Nowa wiadomośćze strony marfloor.pl od ".$name;
+
+    $email_adress = "wojtek.2419@gmail.com";
+
+    mail($email_adress, $theme, $content);
+
+    header("Location: ../index.html");
+?>
